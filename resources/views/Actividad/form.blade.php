5fac0e57658df7cf85606beb43855171792b19e3
Haiden <kevin.haiden96@gmail.com>
2019-12-08 22:22:20 -0400

    act

---
+++ resources/views/Actividad/form.blade.php
@@ -0,0 +1,28 @@
+<div class="modal-body">
+    <input type="hidden" name="id" id="id" class="form-control">
+    <div class="row">
+        <div class="col-6">
+            <label for="tipo">TIPO</label>
+            <select name="tipo" class="form-control" id="tipo" onchange="changeTipo()" required >
+                <option value="1">Departamentos</option>
+                <option value="2">Provincia</option>
+            </select>
+        </div>
+        <div id="divDep" class="col-6" style="display:none">
+            <label for="ubicacion_id">DEPARTAMENTO</label>
+            <select name="ubicacion_id" class="form-control" id="ubicacion_id" required>
+                @foreach($departamentos as $dep)
+                <option value="{{$dep->id}}">{{$dep->nombre}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-3">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" id="nombre" class="form-control" step="any" required>
+        </div>
+    </div>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+    <button type="submit" class="btn btn-primary" id="btn">Editar</button>
+</div>
--- resources/views/Actividad/index.blade.php
+++ resources/views/Actividad/index.blade.php
@@ -1,1 +1,84 @@
-<?php
+@extends('layouts.master')
+@section('title', 'Actividad')
+@section('header-title','Listado de actividades')
+@section('vistas')
+<div class="row">
+    <div class="col-md-3 col-sm-6 col-12 float-sm-right">
+        <div class="info-box bg-success">
+            <span class="info-box-icon"><i class="far fa-flag"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Visitas</span>
+                {{-- <span class="info-box-number">{{$view->vistas}}</span> --}}
+                <span class="info-box-number">{{$view->vistas}}</span>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 70%"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('header-content')
+<div class="row">
+    <div class="col-3">
+        <button class="btn btn-primary btn-lg" id="new" data-toggle="modal" data-target="#edit">Nuevo</button>
+    </div>
+</div>
+@endsection
+@section('content')
+<table id="clientTable" class="table table-bordered table-hover">
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>CODIGO</th>
+        <th>NOMBRE</th>
+        <th>DESCRIPCION</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($ubicaciones as $ubicacion)
+    <tr>
+        <td>{{$ubicacion->id}}</td>
+        <td>{{$ubicacion->codigo}}</td>
+        <td>{{$ubicacion->nombre}}</td>
+        <td>{{$ubicacion->descripcion}}</td>
+        <td >
+            <a class="btn btn-info"
+               {{--data-idcliente="{{$ubicacion->id}}"--}}
+               {{--data-ci="{{$ubicacion->codigo}}" --}}
+               {{--data-nombre="{{$ubicacion->nombre}}"--}}
+               {{--data-apellido_pat="{{$ubicacion->descripcion}}"--}}
+               {{--data-toggle="modal" id="btnedit" data-target="#edit" --}}
+               onclick='editar({{ json_encode($ubicacion) }})'>
+                Edit
+            </a>
+            {!! Form::open(['route' => ['ubicacion.destroy',$ubicacion->id],'method'=>'DELETE','style'=>'display: inline']) !!}
+            {{Form::token()}}
+            <button onclick="return confirm('¿Estas seguro?')" type="submit" class="btn btn-danger">Delete</button>
+            {!! Form::close() !!}
+        </td>
+        @endforeach
+    </tr>
+    </tbody>
+</table>
+<div id="edit" class="modal fade" role="dialog">
+    {!! Form::open(['route' => ['ubicacion.update','0'],'method'=>'PUT','id'=>"ubicacionForm"]) !!}
+    {{Form::token()}}
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="htitle">Editar Ubicacion</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            @include('Ubicacion.form')
+        </div>
+    </div>
+    {!! Form::close() !!}
+</div>
+@push('scripts')
+@include('Ubicacion.modal')
+@endpush
+
+@endsection
